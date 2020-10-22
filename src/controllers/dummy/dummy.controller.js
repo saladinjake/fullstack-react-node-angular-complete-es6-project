1f8993d83b6cbe15309ab817c4eac2f789def6d6
saladinjake <juwavictor@gmail.com>
2020-10-22 22:35:29 +0100

    Route  class and controller get all method created for dummy controller

--- src/controllers/dummy/dummy.controller.js
+++ src/controllers/dummy/dummy.controller.js
@@ -4,27 +4,123 @@ const DUMMY_DATABASE = alldbs.dummydb;
 export class DummyController{
    static getAll(request,response){
 
-     DUMMY_DATABASE.length <=0 ? return response.status(400).json(
+      if( DUMMY_DATABASE.length <=0 ){
+         return response.status(400).json({
+           status: 400,
+           data : [],
+           message : "Empty Resource"
+         }
+       )
+     } else {
+         return response.status(200).json(
+         {
+           status: 200,
+           data : DUMMY_DATABASE,
+           message : "Resource Exists"
+         }
+       )
+     }
+   }
+
+   static getById(request,response){
+     const validId = !Number(request.params.id)
+
+     if(validId) {
+        return response.status(400).json(
        {
          status: 400,
          data : [],
-         message : "Empty Resource"
-       }
-     ) :
-     return response.status(200).json(
-       {
-         status: 200,
-         data : DUMMY_DATABASE,
-         message : "Resource Exists"
+         message : "Invalid ID Resource"
        }
      )
+
+    }else{
+       const result = DummyController.findAndRespond(validId)
+
+       return response.status(200).json(
+         {
+           status: 200,
+           data : result,
+           message : "Resource Exists"
+         }
+       )
+     }
+
+
    }
 
-   static getById(request,response){}
+   static save(request,response){
+     const {title,description} = request.body;
+     DUMMY_DATABASE.push({title,description})
+     return response.status(201).json(
+     {
+      status: 201,
+      data : [{title,description}],
+      message : "Created New Resource"
+     }
+    )
+   }
 
-   static save(request,response){}
+   static update(request,response){
+     const validId = !Number(request.params.id)
 
-   static update(request,response){}
+     if(validId) {
+        return response.status(400).json(
+       {
+         status: 400,
+         data : [],
+         message : "Invalid ID Resource"
+       }
+     )
 
-   static delete(request,response){}
+    }else{
+       const {title,description} = request.body;
+       const result = DummyController.findAndRespond(validId);
+       const index = DUMMY_DATABASE.indexOf(result);
+       const deleteOperation = DUMMY_DATABASE.splice(index,1)
+       DUMMY_DATABASE[index] = {title,description}
+
+       return response.status(200).json(
+         {
+           status: 200,
+           data : deleteOperation,
+           message : "Resource Deleted"
+         }
+       )
+     }
+
+   }
+
+   static delete(request,response){
+     const validId = !Number(request.params.id)
+
+     if(validId) {
+        return response.status(400).json(
+       {
+         status: 400,
+         data : [],
+         message : "Invalid ID Resource"
+       }
+     )
+
+    }else{
+       const result = DummyController.findAndRespond(id);
+       const index = DUMMY_DATABASE.indexOf(result);
+       const deleteOperation = DUMMY_DATABASE.splice(index,1)
+
+       return response.status(200).json(
+         {
+           status: 200,
+           data : deleteOperation,
+           message : "Resource Deleted"
+         }
+       )
+     }
+
+   }
+
+   static findAndRespond(id){
+     const result = DUMMY_DATABASE.filter(item => item.id == id)
+     return result;
+   }
 }
